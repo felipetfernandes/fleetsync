a28e374f91c2602140b9d2ac4749d06b0cf9556e
Felipe Fernandes <108821046+felipetfernandes@users.noreply.github.com>
2025-06-09 12:38:26 -0300

    Update page.tsx

--- frontend/app/orders/page.tsx
+++ frontend/app/orders/page.tsx
@@ -14,7 +14,7 @@ export default function OrdersPage() {
 
   useEffect(() => {
     const fetchOrders = async () => {
-      const data = await fetchClientSide<Order[]>("GET",`/orders`);
+      const data = await fetchClientSide<Order[]>("GET",`/orders?include=vehicles,items,branch,workshop`);
       setOrders(data);
     };
     fetchOrders();

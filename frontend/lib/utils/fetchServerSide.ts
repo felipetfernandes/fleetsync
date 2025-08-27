da6ea58b180e3d8998ea5788c934bee883bad976
Felipe Clemente <sofeshops@gmail.com>
2025-08-26 22:48:49 -0300

    fix: correções de rotas e formulários

--- frontend/lib/utils/fetchServerSide.ts
+++ frontend/lib/utils/fetchServerSide.ts
@@ -1,33 +1,42 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+import { cookies } from "next/headers"
 
-const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_DOCKER_API_URL;
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_DOCKER_API_URL
 
-export async function fetchServerSide<T>(url: string): Promise<T> {
+export async function fetchServerSide<T>(method: string, url: string, body?: any): Promise<T> {
   try {
-    const access_token = cookies().get("access_token")?.value;
+    const access_token = cookies().get("access_token")?.value
 
     if (!access_token) {
-      return redirect("/login");
+      
+      throw new Error("Token de acesso não encontrado")
     }
-    const res = await fetch(NEXT_PUBLIC_BASE_URL + url, {
-      method: "GET",
+
+    const requestOptions: RequestInit = {
+      method,
       credentials: "include",
       next: { revalidate: 120 },
       headers: {
         "Content-Type": "application/json",
         cookie: `access_token=${access_token}`,
       },
-    });
-
-    if (!res.ok) {
-      throw new Error(`Erro ao buscar ${url}: ${res.status} ${res.statusText}`);
     }
 
-    const data = await res.json();
-    return data as T;
+    if (body && method !== "GET") {
+      requestOptions.body = JSON.stringify(body)
+      
+    }
+
+    const res = await fetch(NEXT_PUBLIC_BASE_URL + url, requestOptions)
+
+    if (!res.ok) {
+      const errorText = await res.text()
+      
+      throw new Error(`Erro ao buscar ${url}: ${res.status} ${res.statusText} - ${errorText}`)
+    }
+
+    const data = await res.json()
+    return data as T
   } catch (error) {
-    console.error(`Erro ao buscar dados de ${url}:`, error);
-    throw error;
+    throw error
   }
 }
